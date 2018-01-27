f934d15fd4565f4eb2a0eabfc3a5036529f6aca0
David VanDusen <github@davidvandusen.com>
2018-01-27 15:32:25 -0800

    Load transaction pages and render formatted when complete

--- public/assets/javascript/index.js
+++ public/assets/javascript/index.js
@@ -1,10 +1,37 @@
 (function init() {
 
   const main = function main() {
+    configureHandlebars();
     loadTemplates();
-    show('root', 'loading-template');
+    show('loading-template');
+    fetchConfig()
+      .then(fetchTransactions)
+      .then(showTransactions)
+      .catch(handleError);
   };
   document.addEventListener('DOMContentLoaded', main);
+
+  const configureHandlebars = function configureHandlebars() {
+    Handlebars.registerHelper('formatDateShort', function shortDateFormat(date) {
+      return moment(date).format('MMM Do, YYYY');
+    });
+    Handlebars.registerHelper('formatAccountingInverted', function formatAccountingInverted(value) {
+      const numericValue = Number(value);
+      return formatAccounting(-numericValue);
+    });
+  };
+
+  const formatAccounting = function formatCurrencyForAccounting(value) {
+    const numericValue = Number(value);
+    const isNegative = numericValue < 0;
+    const dollarsAndCents = numericValue.toFixed(2).split('.');
+    dollarsAndCents[0] = Math.abs(Number(dollarsAndCents[0])).toLocaleString();
+    var formatted = '';
+    if (isNegative) formatted += '(';
+    formatted += '$' + dollarsAndCents[0] + '.' + dollarsAndCents[1];
+    if (isNegative) formatted += ')';
+    return formatted;
+  };
 
   const cachedTemplates = {};
   const loadTemplates = function loadTemplatesFromDOM() {
@@ -21,8 +48,55 @@
     return cachedTemplates[templateId](replacements);
   };
 
-  const show = function showRenderedTemplateInElement(rootId, templateId, data) {
-    document.getElementById(rootId).innerHTML = render(templateId, data);
+  const show = function showRenderedTemplateInRoot(templateId, data) {
+    document.getElementById('root').innerHTML = render(templateId, data);
+  };
+
+  const parseJSONResponse = function parseJSONResponse(res) {
+    return res.json();
+  };
+
+  const fetchConfig = function fetchAPIConfigFromLocal() {
+    return fetch('/config.json').then(parseJSONResponse);
+  };
+
+  const getPageURL = function getPageURL(config, pageNumber) {
+    return config['API_TRANSACTIONS_URL'] + pageNumber + config['API_TRANSACTIONS_EXT'];
+  };
+
+  const fetchTransactions = function fetchTransactionDataFromRemoteHost(config) {
+    var combinedTransactions;
+    var currentPage = 1;
+    const fetchPages = function recursivelyFetchPages() {
+      return fetch(getPageURL(config, currentPage))
+        .then(parseJSONResponse)
+        .then(function combineTransactions(transactions) {
+          if (!combinedTransactions) {
+            combinedTransactions = transactions;
+          } else {
+            combinedTransactions.transactions = combinedTransactions.transactions.concat(transactions.transactions);
+          }
+          if (combinedTransactions.transactions.length < combinedTransactions.totalCount) {
+            currentPage++;
+            return fetchPages();
+          } else {
+            return combinedTransactions;
+          }
+        });
+    };
+    return fetchPages();
+  };
+
+  const showTransactions = function showRenderedTransactionsTemplate(transactions) {
+    transactions.total = transactions.transactions.reduce(function sum(s, t) {return s + Number(t.Amount);}, 0);
+    show('transactions-table-template', transactions);
+  };
+
+  const handleError = function showErrorWithTemplate(err) {
+    console.error(err);
+    show('error-template', {
+      message: 'The application encountered an error.'
+    });
   };
 
 })();
